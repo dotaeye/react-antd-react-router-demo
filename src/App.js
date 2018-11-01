65b2342bdcdbfcc0bbdabd70a4de184a21719f13
dotaeye <dotaeye@yeah.net>
2018-11-01 15:34:43 +0800

    add prettierrc

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
-import {Route,Switch,Redirect} from 'react-router-dom'
-import Home from './component/Home'
-import About from './component/About'
-
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Home from './component/Home';
+import About from './component/About';
 
 class App extends Component {
   render() {
-     if(window.location.hash === '#/'){
-         return <Redirect to='/home'/>
-     }
+    if (window.location.hash === '#/') {
+      return <Redirect to="/home" />;
+    }
     return (
       <div className="App">
-          <Switch>
-              <Route path='/home/:id'  component={Home}/>
-              <Route path='/about/:id'  component={About}/>
-              <Route path='/about'  component={About}/>
-              <Route path='/home' component={Home}/>
-          </Switch>
+        <Switch>
+          <Route path="/home/:id" component={Home} />
+          <Route path="/about/:id" component={About} />
+          <Route path="/about" component={About} />
+          <Route path="/home" component={Home} />
+        </Switch>
       </div>
     );
   }
